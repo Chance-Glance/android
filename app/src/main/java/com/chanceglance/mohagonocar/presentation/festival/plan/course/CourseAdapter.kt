5b369126ab5da10fbc6ca12eefcd1845dc3549d8
tn-als <tnals@kyonggi.ac.kr>
2024-10-29 22:50:37 +0900

    feat/travel-plan

---
+++ app/src/main/java/com/chanceglance/mohagonocar/presentation/festival/plan/course/CourseAdapter.kt
@@ -0,0 +1,90 @@
+package com.chanceglance.mohagonocar.presentation.festival.plan.course
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.chanceglance.mohagonocar.R
+import com.chanceglance.mohagonocar.databinding.ItemPlanPlaceBinding
+import com.chanceglance.mohagonocar.databinding.ItemPlanRouteBinding
+
+class CourseAdapter() : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    private val itemList = mutableListOf<CourseItem>()
+
+    companion object {
+        const val TYPE_PLACE = 0
+        const val TYPE_SUBPATH = 1
+        const val TYPE_END_PLACE = 2
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return when (itemList[position]) {
+            is CourseItem.Place -> TYPE_PLACE
+            is CourseItem.SubPath -> TYPE_SUBPATH
+            is CourseItem.EndPlace -> TYPE_END_PLACE
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            TYPE_PLACE -> {
+                val binding = ItemPlanPlaceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                PlaceViewHolder(binding)
+            }
+            TYPE_SUBPATH -> {
+                val binding = ItemPlanRouteBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                SubPathViewHolder(binding)
+            }
+            TYPE_END_PLACE -> {
+                val binding = ItemPlanPlaceBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+                EndPlaceViewHolder(binding)
+            }
+            else -> throw IllegalArgumentException("Invalid view type")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is PlaceViewHolder -> holder.bind(itemList[position] as CourseItem.Place)
+            is SubPathViewHolder -> holder.bind(itemList[position] as CourseItem.SubPath)
+            is EndPlaceViewHolder -> holder.bind(itemList[position] as CourseItem.EndPlace)
+        }
+    }
+
+    override fun getItemCount(): Int = itemList.size
+
+    inner class PlaceViewHolder(private val binding: ItemPlanPlaceBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: CourseItem.Place) {
+            binding.tvPlaceName.text = item.name
+        }
+    }
+
+    inner class SubPathViewHolder(private val binding: ItemPlanRouteBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: CourseItem.SubPath) {
+            if(item.pathType=="BUS"){
+                binding.ivTypeIcon.load(R.drawable.ic_bus_black_24)
+            }else{
+                binding.ivTypeIcon.load(R.drawable.ic_walk_black_24)
+            }
+            binding.tvRouteType.text = item.pathType
+            binding.tvRouteStart.text = item.startPlaceName
+            binding.tvRouteEnd.text = item.endPlaceName
+            binding.tvRouteTime.text = "${item.sectionTime}분"
+        }
+    }
+
+    inner class EndPlaceViewHolder(private val binding: ItemPlanPlaceBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: CourseItem.EndPlace) {
+            binding.tvPlaceName.text = item.name
+        }
+    }
+
+    // 데이터를 설정하고 RecyclerView 갱신하는 메서드 추가
+    fun setData(list: List<CourseItem>) {
+        itemList.clear()
+        itemList.addAll(list)
+        notifyDataSetChanged()
+    }
+
+}
--- app/src/main/java/com/chanceglance/mohagonocar/presentation/festival/plan/course/CourseTextFragment.kt
+++ app/src/main/java/com/chanceglance/mohagonocar/presentation/festival/plan/course/CourseTextFragment.kt
@@ -1,6 +1,7 @@
 package com.chanceglance.mohagonocar.presentation.festival.plan.course
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +12,7 @@ class CourseTextFragment:Fragment() {
     private var _binding:FragmentCourseTextBinding?=null
     private val binding:FragmentCourseTextBinding
         get()= requireNotNull(_binding){"null"}
+    private lateinit var courseAdapter: CourseAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -27,6 +29,20 @@ class CourseTextFragment:Fragment() {
     }
 
     private fun setting(){
+        Log.d("coursetextfragment", "Course Text Fragment Visible")
+        courseAdapter = CourseAdapter()
+        binding.rvRoute.adapter=courseAdapter
 
+        // 데이터를 설정하고 갱신
+        val data = listOf(
+            CourseItem.Place(name = "여행 장소 1"),  // 시작 장소
+            CourseItem.SubPath(pathType = "BUS", startPlaceName = "여행 장소 1", endPlaceName = "버스 정류장1", sectionTime = 10),
+            CourseItem.SubPath(pathType = "BUS", startPlaceName = "버스 정류장 1", endPlaceName = "여행 장소 2", sectionTime = 10),
+            CourseItem.Place(name = "여행 장소 2"), // 중간 장소
+            CourseItem.SubPath(pathType = "WALK", startPlaceName = "여행 장소 2", endPlaceName = "여행 장소 3", sectionTime = 15),
+            CourseItem.EndPlace(name = "여행 장소 3")                // 마지막 장소
+        )
+
+        courseAdapter.setData(data)
     }
 }
